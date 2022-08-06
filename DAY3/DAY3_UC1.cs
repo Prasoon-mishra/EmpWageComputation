1323c390803f090c04fae689bf871589d5b7a3eb
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-06 09:55:32 +0530

    [Prasoon Mishra] Add Employee present or absent

---
+++ DAY3/DAY3_UC1.cs
@@ -0,0 +1,20 @@
+﻿// TO CHECK WHETHER EMPLOYEE IS PRESENT OR NOT
+class DAY3_UC1
+{
+    static void Main(String[] args)
+    {
+        int empPresent = 1;
+        Random r = new Random();
+        int empCheck=r.Next(0,2);
+        if(empCheck==empPresent)
+        {
+            Console.WriteLine("Employee is Present");
+        }
+        else
+        {
+            Console.WriteLine("Employee is not Present");
+        }
+
+    }
+}
+
---
+++ DAY3/EmployeePresentAbsent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY3
+{
+    public class EmployeeParttimefulltime
+    {
+        public int EmpPresent()
+        {
+            int EmpWagePerHour = 20;
+            int DailyWage;
+            int EmpHrs = 0;
+            Random random = new Random();
+            int ch = random.Next(0, 2);
+            int EmpFullTime = 1;
+            int EmpPartTime = 2;
+
+           if (ch == EmpFullTime)
+           {
+            Console.WriteLine("Employee is full time Presnt");
+            EmpHrs = 8;
+           }
+           else if (ch == EmpPartTime)
+           {
+            Console.WriteLine("Employee is Part time Present");
+            EmpHrs = 4;
+           }
+           else
+           {
+            Console.WriteLine("Employee is absent");
+            EmpHrs = 0;
+           }
+           DailyWage = EmpWagePerHour * EmpHrs;
+           return DailyWage;
+
+        }
+    }
+}
